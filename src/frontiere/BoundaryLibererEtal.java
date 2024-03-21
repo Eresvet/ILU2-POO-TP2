dba3cde64ee0bb9b430ae60986f92a187c46e248
Eresvet <remi32450@gmail.com>
2024-03-21 19:56:04 +0100

    Faire 7 - AfficherMarche

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -10,7 +10,18 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		boolean vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
+		if(!vendeurReconnu) {
+			System.out.println("Mais vous n'etes pas inscrit sur notre marche aujour'hui !\n");
+		}else {
+			String[] donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
+			if(donneesEtal[0].equals("true")) {
+				System.out.println("Vous avez vendu "+donneesEtal[4]+" sur " +
+						donneesEtal[3] + " " + donneesEtal[2] + ".");
+				
+				System.out.println("Au revoir "+nomVendeur+", passez une bonne journee.\n");
+			}
+		}
 	}
 
 }
