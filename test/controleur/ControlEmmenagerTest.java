76483f5dd07796985415efdc709285df22e2958e
Eresvet <remi32450@gmail.com>
2024-04-04 17:51:01 +0200

    tests début controlemmenager

---
+++ test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,56 @@
+package controleur;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irreductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+
+	@Test
+	void testControlEmmenager() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertNotNull("Constructeur ne renvoie pas null", controlEmmenager);
+	}
+
+	@Test
+	void testIsHabitant() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		assertFalse(controlEmmenager.isHabitant("Pas habitant"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+	}
+
+	@Test
+	void testAjouterDruide() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		//pas de test
+	}
+
+	@Test
+	void testAjouterGaulois() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		//pas de test
+	}
+
+}
